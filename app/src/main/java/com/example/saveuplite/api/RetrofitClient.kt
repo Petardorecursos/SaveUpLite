11946098b26fafcb5f62ab1d2e1611fe03742e7d
ItsMilkey <han.gomez@duocuc.cl>
2025-11-12 23:43:17 -0300

    Cambia url de RetrofitClient para apuntar a Render

--- app/src/main/java/com/example/saveuplite/api/RetrofitClient.kt
+++ app/src/main/java/com/example/saveuplite/api/RetrofitClient.kt
@@ -1,7 +1,5 @@
 package com.example.saveuplite.api
 
-// Importa la interfaz ApiService del MISMO paquete
-import com.example.saveuplite.api.ApiService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,14 +8,10 @@ import retrofit2.converter.gson.GsonConverterFactory
  */
 object RetrofitClient {
 
-    // URL base del backend.
-    // IMPORTANTE:
-    // - Usa "http://10.0.2.2:8080/" para el emulador de Android.
-    // - Usa la IP de tu computador en la red local para un dispositivo físico.
-    private const val BASE_URL = "http://192.168.18.8:8080/"
+    // URL base del backend desplegado en Render.
+    private const val BASE_URL = "https://lite-backend-1wn9.onrender.com/"
 
     // Creación de la instancia de Retrofit usando un inicializador "lazy".
-    // Esto asegura que la instancia se cree solo una vez, la primera vez que se accede a ella.
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
